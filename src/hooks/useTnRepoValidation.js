5352f5a53dd0250e655303de6fdd82c4e5011b68
mandolyte <cecil.new@gmail.com>
2021-11-15 09:19:34 -0500

    refactor tn hook 1

--- src/hooks/useTnRepoValidation.js
+++ src/hooks/useTnRepoValidation.js
@@ -16,19 +16,27 @@ import {decodeBase64ToUtf8} from '@utils/decode'
 
 
 export default function useTnRepoValidation({authentication, owner, server, languageId}) {
-
+/*
   const [tnRepoTree, setTnRepoTree] = useState({})
   const [tnRepoTreeManifest, setTnRepoTreeManifest] = useState({})
   const [tnRepoTreeErrorMessage, setTnRepoTreeErrorMessage] = useState(null)
-
+*/
+  const [{tnRepoTree, 
+    tnRepoTreeManifest, 
+    tnRepoTreeErrorMessage}, 
+    setValues
+  ] = useState({tnRepoTree:null, tnRepoTreeManifest:null, tnRepoTreeErrorMessage:null})
   // Translation Notes Hook
   // Example: https://qa.door43.org/api/v1/repos/vi_gl/vi_tn/git/trees/master?recursive=true&per_page=99999
   useEffect(() => {
     async function getReposTrees() {
       const url = `${server}/api/v1/repos/${owner}/${languageId}_tn/git/trees/master?recursive=false&per_page=999999`
       let errorCode = 0
+      let _tree = null
+      let _manifest = null
+      let _errorMessage = null
       try {
-        const tnTree = await doFetch(url,
+        const trees = await doFetch(url,
           authentication, HTTP_GET_MAX_WAIT_TIME)
           .then(response => {
             if (response?.status !== 200) {
@@ -37,54 +45,58 @@ export default function useTnRepoValidation({authentication, owner, server, lang
               return null
             }
             return response?.data
-          })
+        })
 
-          if (tnTree === null) { // if no repo
-            console.warn(`AdminContext - empty repo`)
-            setTnRepoTreeErrorMessage("Repo Not Found")
-          } else {
-            setTnRepoTreeErrorMessage(null)
-          }
-          if ( tnTree.tree ) {
-            setTnRepoTree(tnTree.tree)
-            let _url;
-            for (let i=0; i < tnTree.tree.length; i++) {
-              if (tnTree.tree[i].path === "manifest.yaml") {
-                _url = tnTree.tree[i].url
-                break
-              }
+        if (trees === null) { // if no repo
+          console.warn(`AdminContext - empty repo`)
+          //setTnRepoTreeErrorMessage("Repo Not Found")
+          //setValues({tnRepoTreeErrorMessage: "Repo not found"})
+          _errorMessage = "Repo not found"
+        } if ( trees.tree ) {
+          //setTnRepoTree(tnTree.tree)
+          _tree = trees.tree
+          let _url;
+          for (let i=0; i < _tree.length; i++) {
+            if (_tree.tree[i].path === "manifest.yaml") {
+              _url = _tree.tree[i].url
+              break
             }
-            if ( _url ) {
-              // get the manifest
-              const tnManifest = await doFetch(_url,
-                authentication, HTTP_GET_MAX_WAIT_TIME)
-                .then(response => {
-                  if (response?.status !== 200) {
-                    errorCode = response?.status
-                    console.warn(`AdminContext - error fetching tn manifest, status code ${errorCode}`)
-                    return null
-                  }
-                  return response?.data
-              })
-              if ( tnManifest === null ) {
-                setTnRepoTreeErrorMessage("Unable to retrieve manifest")
-              } else {
-                if (tnManifest.content && (tnManifest.encoding === 'base64')) {
-                  const _content = decodeBase64ToUtf8(tnManifest.content)
-                  const manifestObj = YAML.safeLoad(_content)
-                  setTnRepoTreeManifest(manifestObj)
-                } else {
-                  setTnRepoTreeErrorMessage("Unable to decode manifest")
+          }
+          if ( _url ) {
+            // get the manifest
+            const __manifest = await doFetch(_url,
+              authentication, HTTP_GET_MAX_WAIT_TIME)
+              .then(response => {
+                if (response?.status !== 200) {
+                  errorCode = response?.status
+                  console.warn(`AdminContext - error fetching tn manifest, status code ${errorCode}`)
+                  return null
                 }
-              }
+                return response?.data
+            })
+            if ( __manifest === null ) {
+              //setTnRepoTreeErrorMessage("Unable to retrieve manifest")
+              _errorMessage = "Unable to retrieve manifest"
             } else {
-              setTnRepoTreeErrorMessage("No manifest found")
-              console.log("no manifest found")
+              if (__manifest.content && (__manifest.encoding === 'base64')) {
+                const _content = decodeBase64ToUtf8(__manifest.content)
+                const manifestObj = YAML.safeLoad(_content)
+                //setTnRepoTreeManifest(manifestObj)
+                _manifest = manifestObj
+              } else {
+                //setTnRepoTreeErrorMessage("Unable to decode manifest")
+                _errorMessage = "Unable to decode manifest"
+              }
             }
           } else {
-            setTnRepoTreeErrorMessage("No files in repo")
+            //setTnRepoTreeErrorMessage("No manifest found")
+            _errorMessage = "No manifest found"
           }
-  
+        } else {
+          //setTnRepoTreeErrorMessage("No files in repo")
+          _errorMessage = "No files in repo"
+        }
+        setValues({tnRepoTree: _tree, tnManifest: _manifest, tnRepoTreeErrorMessage: _errorMessage})
       } catch (e) {
         const message = e?.message
         const disconnected = isServerDisconnected(e)
