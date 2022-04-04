d945b64ee9fe1ead6edf39bc2f5648598245e7e3
mandolyte <cecil.new@gmail.com>
2022-04-04 08:35:37 -0400

    WIP release repo

--- pages/releaseRepo.js
+++ pages/releaseRepo.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import { AuthenticationContext } from 'gitea-react-toolkit'
 import Layout from '@components/Layout'
 import ReleaseRepoSettings from '@components/ReleaseSettings'
@@ -9,6 +10,10 @@ import { AdminContext } from '@context/AdminContext'
 
 const ReleasePage = () => {
   const router = useRouter()
+  const [confirmRelease, setConfirmRelease] = useState(false)
+  const [releaseMessage, setReleaseMessage] = useState(<CircularProgress />)
+  const [releaseActive, setReleaseActive] = useState(false)
+
   const { state: authentication } = useContext(AuthenticationContext)
   const {
     state: {
@@ -23,13 +28,39 @@ const ReleasePage = () => {
       releaseResource,
       releaseVersion,
     },
+    actions: {
+      setReleaseResource,
+      setReleaseVersion,
+    }
   } = useContext(AdminContext)
 
-
   useEffect( () => {
+    if ( releaseResource && releaseVersion ) { 
+      setReleaseActive(true) 
+    } else {
+      setReleaseActive(false)
+    }
+  }, [releaseResource, releaseVersion])
+
+  //  
+  useEffect( () => {
+    if ( !confirmRelease ) return;
+    if ( !releaseResource ) return;
+    if ( !releaseVersion ) return;
+
+    setReleaseMessage(<CircularProgress />)
+
     console.log("Repo to Release:",`${organization}/${languageId}_${releaseResource.id}`)
     console.log("Version to release:", releaseVersion)
-  }, [organization, languageId, releaseResource, releaseVersion])
+
+    setTimeout( () => {
+      setReleaseMessage(<>Success!</>)
+      // initialize release state vars
+      setReleaseResource(null)
+      setReleaseVersion(null)
+      // setConfirmRelease(false)
+    }, 5000)
+  }, [organization, languageId, releaseResource, releaseVersion, confirmRelease])
 
 
 
@@ -54,11 +85,20 @@ const ReleasePage = () => {
               color='primary'
               className='my-3 mx-1'
               variant='contained'
-              onClick={() => router.push('/')}
+              disabled={!releaseActive}
+              onClick={
+                () => {
+                  setConfirmRelease(true)
+                }
+              }
             >
               Release Repository
             </Button>
+            <br/>
           </div>
+          {confirmRelease &&
+            <h1 className='mx-4'>Status: {releaseMessage}</h1>         
+          }
         </div>
       </div>
     </Layout>
