8b79db9a1f1f932124c08a6c1e49ef0424cc7f55
mandolyte <cecil.new@gmail.com>
2022-08-29 10:46:38 -0400

    added back latestReleaseVersion()

--- src/utils/dcsApis.js
+++ src/utils/dcsApis.js
@@ -12,6 +12,55 @@ import {
 } from '@common/BooksOfTheBible'
 import { isServerDisconnected } from './network'
 
+
+/**
+ * determine latest release version
+ * @param {string} server
+ * @param {string} organization
+ * @param {string} languageId
+ * @param {string} resourceId
+ * @return {object} 
+ *                 shape of return object is {isValid: bool, message: string}
+ */
+ async function latestReleaseVersion({server, organization, languageId, resourceId}) {
+  // example:
+  // https://qa.door43.org/api/v1/repos/es-419_gl/es-419_tn/releases?draft=false&pre-release=false&page=1&limit=9999'
+  const uri = server + "/" + Path.join('api','v1','repos',organization,`${languageId}_${resourceId}`,'releases?page=1&limit=9999') ;
+  let val = {}
+  try {
+    const response = await doFetch(uri)
+    if (response.status === 200) {
+      // master branch is in the catalog, thus must have a valid manifest
+      // now fetch the latest release!
+      val.isValid = true
+      if ( response.data.length === 0 ) {
+        val.message = 'No releases yet, use "v1"'
+      } else {
+        val.message = response.data[0]['tag_name']
+      }
+    } else if (response.status === 404) {
+      val.isValid = false
+      val.message = `Repo does not exist: ${languageId}_${resourceId}`
+    } else {
+      val.isValid = false
+      val.message = "Unexpected status returned:"+response.status
+    }
+  } catch (e) {
+    const message = e?.message
+    const disconnected = isServerDisconnected(e)
+    console.warn(`latestReleaseVersion() - error fetching releases,
+      message '${message}',
+      disconnected=${disconnected},
+      url ${uri}
+      Error:`, 
+      e)
+    val.isValid = false
+    val.message = `Network Error: Disconnected=${disconnected}, Error: ${message}`
+  }
+  return val
+}
+
+
 export function getResourceIdFromRepo(repo) {
   let resourceId = repo.split('_')[1]
 
