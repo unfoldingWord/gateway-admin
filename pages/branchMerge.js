3d4fcd64ed9e9cd6532e4cfb6745312ebc769b10
mandolyte <cecil.new@gmail.com>
2023-03-29 08:11:04 -0400

    WIP added radio btns

--- pages/branchMerge.js
+++ pages/branchMerge.js
@@ -1,17 +1,40 @@
 import { useContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from 'translation-helps-rcl/dist/components/Paper'
+
+import {
+  Grid, Link, Box, Divider, Container,
+} from '@material-ui/core'
+import Checkbox from '@material-ui/core/Checkbox'
 import Button from '@material-ui/core/Button'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
 import { AuthenticationContext } from 'gitea-react-toolkit'
 import Layout from '@components/Layout'
-import ReleaseSettings from '@components/ReleaseSettings'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControl from '@material-ui/core/FormControl'
+import FormLabel from '@material-ui/core/FormLabel'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
 import { StoreContext } from '@context/StoreContext'
 import { AdminContext } from '@context/AdminContext'
-import { resourceIdMapper } from '@common/ResourceList'
-import { ALL, OBS_SN, SN, SQ, TN, TQ, TWL, LT, ST, OBS, OBS_SQ, OBS_TW, OBS_TWL, OBS_TA, OBS_TN, OBS_TQ, TW, TA } from "@common/constants";
+import { resourceSelectList, resourceIdMapper } from '@common/ResourceList'
+
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    minWidth: '100%',
+  },
+}))
 
 const BranchMerge = () => {
+  const classes = useStyles()
+
   const router = useRouter()
+
+  const [resourceState, setResourceState] = useState('')
 
   const { state: authentication } = useContext(AuthenticationContext)
   const {
@@ -23,70 +46,79 @@ const BranchMerge = () => {
   } = useContext(StoreContext)
 
   const {
-    state: {
-      releaseResources,
-      releaseNotes,
-      releaseName,
-      releaseState,
-      releaseBooks,
-      obsRepoTree,
-      obsTnRepoTree,
-      obsTwlRepoTree,
-      obsTqRepoTree,
-      obsTaRepoTree,
-      obsTwRepoTree,
-      obsSnRepoTree,
-      obsSqRepoTree,
-      tnRepoTree,
-      twlRepoTree,
-      ltRepoTree,
-      stRepoTree,
-      tqRepoTree,
-      sqRepoTree,
-      snRepoTree,
-      taRepoTree,
-      twRepoTree,
-    },
-    actions: {
-      setReleaseResources,
-      setReleaseNotes,
-      setReleaseName,
-      setReleaseState,
-      setReleaseBooks,
-      setRefresh,
-    },
+    state: { releaseResources },
+    actions: { setReleaseResources },
   } = useContext(AdminContext)
 
+  const resources = resourceSelectList()
+
+  const handleResourceChange = (event) => {
+    const item = event.target.name
+    const isChecked = event.target.checked
+    const newMap = new Map(releaseResources)
+
+    if ( isChecked ) {
+      newMap.set(item, isChecked)
+      prepResourceForRelease(item)
+    } else {
+      newMap.delete(item)
+    }
+    setReleaseResources(newMap)
+  }
+
+
   return (
-    <Layout>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col w-full px-4 lg:w-132 lg:p-0'>
-          <h1 className='mx-4'>No Conflict Merge Management</h1>
-          <p>here is where a resource list goes</p>
-          <p>here is where a branch picker goes</p>
-          <div className='flex justify-end'>
-            <Button
-              size='large'
-              color='primary'
-              className='my-3 mx-1'
-              variant='contained'
-            >
+    <>
+      <Layout>
+        <Paper>
+          <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col w-full px-4 lg:w-132 lg:p-0'>
+              <h1 className='mx-4'>No Conflict Merge Management</h1>
+              <FormControl required component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Select a Resource</FormLabel>
+                <FormGroup>
+                  <RadioGroup
+                    aria-labelledby="release-type-radio-buttons-group-label"
+                    defaultValue="prod"
+                    name="release-type-radio-buttons-group"
+                    row
+                    value={resourceState}
+                    onChange={handleResourceChange}
+                  >
+                    {resources.map( ({ id,name }) =>
+                      <FormControlLabel key={id} value={id} control={<Radio />} label={name} />,
+                    )}
+                  </RadioGroup>
+
+                </FormGroup>
+                <FormHelperText />
+              </FormControl>
+              <p>here is where a branch picker goes</p>
+              <div className='flex justify-end'>
+                <Button
+                  size='large'
+                  color='primary'
+                  className='my-3 mx-1'
+                  variant='contained'
+                >
               Update user branch with new stuff from Master
-            </Button>
-            <Button
-              size='large'
-              color='primary'
-              className='my-3 mx-1'
-              variant='contained'
-            //   disabled={!releaseActive}
-            >
+                </Button>
+                <Button
+                  size='large'
+                  color='primary'
+                  className='my-3 mx-1'
+                  variant='contained'
+                  //   disabled={!releaseActive}
+                >
               Merge user branch into Master!
-            </Button>
-            <br/>
+                </Button>
+                <br/>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </Layout>
+        </Paper>
+      </Layout>
+    </>
   )
 }
 
